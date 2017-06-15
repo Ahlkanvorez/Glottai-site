c3b6d7c552566e6772fa381a1271144f8426a21b
Robert Mitchell <robert.mitchell36@gmail.com>
2017-06-15 13:28:50 -0500

    Completed a template for tables on the grammar page, with local dummy data.

--- app/grammar/grammar.component.js
+++ app/grammar/grammar.component.js
@@ -5,15 +5,48 @@
         templateUrl: 'grammar/grammar.template.html',
         controller: ['$scope',
             function ($scope) {
-                $scope.tables = {
+                // TODO: Pull this from a server using the async/await commands.
+                const tables = {
                     declensions: {
                         first: {
                             name: "First Declension",
-                            theme: "-ā-",
-                            genders: ["f"],
-                            table: [
-                                "-a", "-ae", "-ae", "-am", "-ā", "-a",
-                                "-ae", "-ārum", "-īs", "-ās", "-īs", "-ae"
+                            tables: [
+                                {
+                                    gender: "f.",
+                                    data: [
+                                        ["Nom./Voc.", "-a", "-ae"],
+                                        ["Gen.", "-ae", "-ārum"],
+                                        ["Dat.", "-ae", "-īs"],
+                                        ["Acc.", "-am",  "-ās"],
+                                        ["Abl.", "-ā",  "-īs"]
+                                    ]
+                                }
+                            ]
+                        },
+                        second: {
+                            name: "Second Declension",
+                            tables: [
+                                {
+                                    gender: "m.",
+                                    data: [
+                                        ["Nom.", "-us/er", "-ī"],
+                                        ["Gen.", "-ī", "-ōrum"],
+                                        ["Dat.", "-ō", "-īs"],
+                                        ["Acc.", "-um", "-ōs"],
+                                        ["Abl.", "-ō", "-īs"],
+                                        ["Voc.", "-e/er", "-ī"]
+                                    ]
+                                },
+                                {
+                                    gender: "n.",
+                                    data: [
+                                        ["Nom./Voc.", "-um", "-a"],
+                                        ["Gen.", "-ī", "-ōrum"],
+                                        ["Dat.", "-ō", "-īs"],
+                                        ["Acc.", "-um", "-a"],
+                                        ["Abl.", "-ō", "-īs"]
+                                    ]
+                                }
                             ]
                         }
                     },
@@ -21,26 +54,60 @@
                         first: {
                             name: "First Conjugation",
                             theme: "-ā-",
-                            tense: "Present",
-                            mood: "Indicative",
-                            voice: "Active",
-                            table: [
-                                ["-ō", "-ās", "-at"],
-                                ["-āmus", "-atis", "-ant"]
+                            tables: [
+                                {
+                                    tense: "Present",
+                                    mood: "Indicative",
+                                    voice: "Active",
+                                    data: [
+                                        ["1st", "-ō", "-āmus"],
+                                        ["2nd", "-ās",  "-ātis"],
+                                        ["3rd", "-at", "-ant"]
+                                    ]
+                                },
+                                {
+                                    tense: "Future",
+                                    mood: "Indicative",
+                                    voice: "Active",
+                                    data: [
+                                        ["1st", "-ābō", "-ābimus"],
+                                        ["2nd", "-ābis", "-ābitis"],
+                                        ["3rd", "-ābit", "-ābunt"]
+                                    ]
+                                }
+                            ]
+                        },
+                        second: {
+                            name: "Second Conjugation",
+                            theme: "-ē-",
+                            tables: [
+                                {
+                                    tense: "Present",
+                                    mood: "Indicative",
+                                    voice: "Active",
+                                    data: [
+                                        ["1st", "-eō", "-ēmus"],
+                                        ["2nd", "-ēs", "-ētis"],
+                                        ["3rd", "-et", "-ent"]
+                                    ]
+                                },
+                                {
+                                    tense: "Future",
+                                    mood: "Indicative",
+                                    voice: "Active",
+                                    data: [
+                                        ["1st", "-ēbō", "-ēbimus"],
+                                        ["2nd", "-ēbis", "-ēbitis"],
+                                        ["3rd", "-ēbit", "-ēbunt"]
+                                    ]
+                                }
                             ]
                         }
                     }
                 };
 
-                $scope.tableData = [];
-
-                $scope.displayConjugation = function (data) {
-
-                };
-
-                $scope.displayDeclension = function (data) {
-
-                };
+                $scope.conjugations = [ tables.conjugations.first, tables.conjugations.second ];
+                $scope.declensions = [ tables.declensions.first, tables.declensions.second ];
             }
         ]
     });
