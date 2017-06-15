b31a60c4864b6165c319a8c6e17b482ef8d4f40b
Robert Mitchell <robert.mitchell36@gmail.com>
2017-06-15 10:23:10 -0500

    Updated the package to use yarn instead of npm, and started writing a grammar view which would allow studying of conjugation and declension tables, as well as other grammatical rules.

--- app/grammar/grammar.component.js
+++ app/grammar/grammar.component.js
@@ -3,9 +3,44 @@
 
     angular.module('grammar').component('grammar', {
         templateUrl: 'grammar/grammar.template.html',
-        controller: [
-            function () {
+        controller: ['$scope',
+            function ($scope) {
+                $scope.tables = {
+                    declensions: {
+                        first: {
+                            name: "First Declension",
+                            theme: "-ā-",
+                            genders: ["f"],
+                            table: [
+                                "-a", "-ae", "-ae", "-am", "-ā", "-a",
+                                "-ae", "-ārum", "-īs", "-ās", "-īs", "-ae"
+                            ]
+                        }
+                    },
+                    conjugations: {
+                        first: {
+                            name: "First Conjugation",
+                            theme: "-ā-",
+                            tense: "Present",
+                            mood: "Indicative",
+                            voice: "Active",
+                            table: [
+                                ["-ō", "-ās", "-at"],
+                                ["-āmus", "-atis", "-ant"]
+                            ]
+                        }
+                    }
+                };
 
+                $scope.tableData = [];
+
+                $scope.displayConjugation = function (data) {
+
+                };
+
+                $scope.displayDeclension = function (data) {
+
+                };
             }
         ]
     });
