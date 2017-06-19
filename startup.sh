9809fb012cf3e7dc280e1251bda5d470732ae7ad
Robert Mitchell <robert.mitchell36@gmail.com>
2017-06-19 10:07:17 -0500

    Switched from CSS to SCSS, and updated the startup script accordingly.

--- startup.sh
+++ startup.sh
@@ -3,5 +3,8 @@
 echo "Conglomerating files for distribution ..."
 gulp js
 
+echo "Compiling SCSS to CSS ..."
+sass app/stylesheets/app.scss app/stylesheets/app.css
+
 echo "Updating packages with yarn, and starting the server ..."
 npm start
